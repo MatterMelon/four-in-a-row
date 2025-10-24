c84b869e3bb3b591dac144adc31bc4f90bd9bb12
Andrey Popov <dil2232@gmail.com>
2025-10-24 13:53:51 +0600

    feat: Marker move to the selected column

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Column from '../Column/Column';
+import Marker from '../Marker/Marker';
 import styles from './Board.module.css';
 
 const initColumnState: SlotState[] = [0, 0, 0, 0, 0, 0];
@@ -12,9 +13,64 @@ export default function Board({ rows, cols }: BoardProps) {
   const [boardState, setBoardState] = useState<SlotState[][]>(
     new Array(cols).fill(initColumnState)
   );
+  // const [activeColumn, setActiveColumn] = useState<number | null>(null);
+  const [columnPositions, setColumnPositions] = useState<number[]>([]);
+
+  const boardRef = useRef<HTMLDivElement>(null);
+  const columnRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const markerRef = useRef<HTMLDivElement>(null);
+
+  // Инициализация refs для колонок
+  useEffect(() => {
+    console.log('Init column refs...');
+    columnRefs.current = columnRefs.current.slice(0, cols);
+    console.log(`Column refs: ${columnRefs.current}`);
+  }, [cols]);
+
+  // Вычисление позиций при изменении размера окна
+  useEffect(() => {
+    const calculatePositions = () => {
+      console.log('RECALCULATING');
+
+      if (!boardRef.current || columnRefs.current.length === 0) {
+        console.error(boardRef.current, columnRefs.current.length);
+        return;
+      }
+
+      const boardRect = boardRef.current.getBoundingClientRect();
+      const markerRect = markerRef.current?.getBoundingClientRect();
+      const positions = columnRefs.current.map((column) => {
+        if (!column || !markerRect) return 0;
+        const columnRect = column.getBoundingClientRect();
+
+        return columnRect.left - boardRect.left + columnRect.width / 2 - markerRect.width / 2;
+      });
+
+      setColumnPositions(positions);
+    };
+
+    calculatePositions();
+
+    window.addEventListener('resize', calculatePositions);
+
+    return () => {
+      window.removeEventListener('resize', calculatePositions);
+    };
+  }, []);
+
+  const moveMarker = (columnNumber: number) => {
+    if (markerRef.current && columnPositions[columnNumber] !== undefined) {
+      markerRef.current.style.transform = `translateX(${-columnPositions[columnNumber]}px)`;
+    }
+  };
+
+  moveMarker(0);
+
+  const handleColumnHover = (columnNumber: number) => moveMarker(columnNumber);
 
   const handleColumnClick = (columnNumber: number) => {
     console.log(`Colunmn ${columnNumber + 1} has been clicked!`);
+
     setBoardState((prevState) => {
       return prevState.map((col, index) => {
         if (index === columnNumber) {
@@ -26,16 +82,23 @@ export default function Board({ rows, cols }: BoardProps) {
   };
 
   return (
-    <div className={styles.board}>
-      {Array.from({ length: cols }).map((_, i) => (
-        <Column
-          columnState={boardState[i]}
-          key={i}
-          columnNumber={i}
-          slotsCount={rows}
-          handleClick={handleColumnClick}
-        />
-      ))}
-    </div>
+    <>
+      <Marker ref={markerRef} />
+      <div ref={boardRef} className={styles.board}>
+        {Array.from({ length: cols }).map((_, i) => (
+          <Column
+            ref={(el) => {
+              columnRefs.current[i] = el;
+            }}
+            columnState={boardState[i]}
+            key={i}
+            columnNumber={i}
+            slotsCount={rows}
+            handleClick={handleColumnClick}
+            handleHover={handleColumnHover}
+          />
+        ))}
+      </div>
+    </>
   );
 }

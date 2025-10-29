6797611a430dfd5e4996b6ce9a7cd6afbda06aaf
Andrey Popov <dil2232@gmail.com>
2025-10-29 15:25:51 +0600

    refactor: use useColumnPositions hook

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import useOnResize from '../../hooks/basic/useOnResize';
+import { useEffect, useRef } from 'react';
 import { useBoardState } from '../../hooks/useBoardState';
 import { useColumnNavigation } from '../../hooks/useColumnNavigation';
+import useColumnPostions from '../../hooks/useColumnPositions';
 import { useMoveMarker } from '../../hooks/useMoveMarker';
 import Column from '../Column/Column';
 import Marker from '../Marker/Marker';
@@ -15,44 +15,24 @@ type BoardProps = {
 export default function Board({ rows, cols }: BoardProps) {
   const { boardState } = useBoardState(rows, cols);
 
-  const [columnPositions, setColumnPositions] = useState<number[]>([]);
-
   const boardRef = useRef<HTMLDivElement>(null);
   const columnRefs = useRef<(HTMLDivElement | null)[]>([]);
   const markerRef = useRef<HTMLDivElement>(null);
 
+  const { columnPositions, calculatePositions, updateColumnRefs } = useColumnPostions(
+    boardRef,
+    columnRefs,
+    markerRef,
+    cols
+  );
+
   const { moveMarker } = useMoveMarker(markerRef, columnPositions);
+
   const { activeColumn, handleColumnHover, handleColumnMouseOut, centerMarker } =
     useColumnNavigation(cols, moveMarker);
 
-  useEffect(() => {
-    columnRefs.current = columnRefs.current.slice(0, cols);
-  }, [cols]);
-
-  const calculatePositions = useCallback(() => {
-    if (!boardRef.current || columnRefs.current.length === 0) {
-      console.error(boardRef.current, columnRefs.current.length);
-      return;
-    }
-
-    const boardRect = boardRef.current.getBoundingClientRect();
-    const markerRect = markerRef.current?.getBoundingClientRect();
-    const positions = columnRefs.current.map((column) => {
-      if (!column || !markerRect) return 0;
-      const columnRect = column.getBoundingClientRect();
-
-      return columnRect.left - boardRect.left + columnRect.width / 2 - markerRect.width / 2;
-    });
-
-    setColumnPositions(positions);
-  }, []);
-
-  useOnResize(calculatePositions);
-
-  useEffect(() => {
-    calculatePositions();
-  }, [calculatePositions]);
-
+  useEffect(() => updateColumnRefs(), [updateColumnRefs]);
+  useEffect(() => calculatePositions(), [calculatePositions]);
   useEffect(() => centerMarker, [centerMarker]);
 
   const handleColumnClick = (columnNumber: number) => {

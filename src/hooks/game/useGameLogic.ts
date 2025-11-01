e64c317b70e8ddca5e8cfbea7abba0023306b927
Andrey Popov <dil2232@gmail.com>
2025-11-01 20:35:28 +0600

    feat: added hook for game logic

---
+++ src/hooks/game/useGameLogic.ts
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react';
+import { SlotState } from '../../types/slotState';
+import { useGameMoves } from './useGameMoves';
+import { useWinCheck } from './useWinCheck';
+
+export function useGameLogic(
+  boardState: SlotState[][],
+  updateColumn: (col: number, state: SlotState[]) => void
+) {
+  const [activePlayer, setActivePlayer] = useState(1);
+  const { makeMove } = useGameMoves(boardState, activePlayer, updateColumn);
+  const { checkWinCondition } = useWinCheck(boardState);
+
+  const switchPlayer = useCallback(() => {
+    if (activePlayer === 1) {
+      setActivePlayer(2);
+    } else {
+      setActivePlayer(1);
+    }
+  }, [activePlayer]);
+
+  const handlePlayerMove = useCallback(
+    (columnNumber: number) => {
+      const newBoardState = makeMove(columnNumber);
+      if (!newBoardState) return;
+
+      const winner = checkWinCondition(newBoardState);
+      if (winner) {
+        console.log(`Побелил игрок ${winner}`);
+        return;
+      }
+
+      switchPlayer();
+    },
+    [checkWinCondition, makeMove, switchPlayer]
+  );
+
+  return { handlePlayerMove };
+}

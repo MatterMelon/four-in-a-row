aa56c35af7df04e7fa9d5cc83e8082317d549ac1
Andrey Popov <dil2232@gmail.com>
2025-11-04 15:03:02 +0600

    refactor: setting last moves + refactor

--- src/hooks/game/useGameMoves.ts
+++ src/hooks/game/useGameMoves.ts
@@ -4,18 +4,23 @@ import gameStore from '../../stores/game.store';
 import { SlotState } from '../../types/slotState';
 
 export function useGameMoves() {
+  const getFirstAvailableSlot = (columnState: SlotState[]) => {
+    const firstNonEmptySlot = columnState.findIndex((slot) => slot !== SlotState.EMPTY);
+    const availableSlot = firstNonEmptySlot === -1 ? columnState.length - 1 : firstNonEmptySlot - 1;
+    return availableSlot;
+  };
+
   const makeMove = useCallback((columnNumber: number) => {
-    const currentColumnState = boardStore.boardState[columnNumber];
-    const firstNonEmptySlot = currentColumnState.findIndex((slot) => slot !== SlotState.EMPTY);
-    const availableSlot =
-      firstNonEmptySlot === -1 ? currentColumnState.length - 1 : firstNonEmptySlot - 1;
+    const columnState = boardStore.boardState[columnNumber];
+    const availableSlot = getFirstAvailableSlot(columnState);
 
     if (availableSlot < 0) return null;
 
-    const newColumnState = [...currentColumnState];
+    const newColumnState = [...columnState];
     newColumnState[availableSlot] =
       gameStore.activePlayer === 1 ? SlotState.PLAYER_ONE : SlotState.PLAYER_TWO;
 
+    gameStore.lastMove = [boardStore.rows - availableSlot - 1, columnNumber];
     boardStore.updateColumn(columnNumber, newColumnState);
 
     const newBoardState = [...boardStore.boardState];

4b47e50ab7ebe2cc69d40cf2ea70bf6ba49ba749
Andrey Popov <dil2232@gmail.com>
2025-11-04 22:29:45 +0600

    feat: added restart button

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,12 +3,14 @@ import { useStore } from '../../stores/root.store';
 import Board from '../Board/Board';
 
 export const Game = observer(() => {
-  const { gameStore } = useStore();
+  const { gameStore, boardStore } = useStore();
+  const { rows, cols } = boardStore.boardSize;
   return (
     <>
       <p>Ходит: Игрок {gameStore.activePlayer}</p>
       {gameStore.winner === 0 ? <h1>Ниья!</h1> : ''}
       {gameStore.winner ? <h1>Победил игрок {gameStore.winner}!</h1> : ''}
+      <button onClick={() => gameStore.startNewGame(rows, cols)}>Рестарт</button>
 
       <Board rows={6} cols={7} />
     </>

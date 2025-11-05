ac897ee1e97ae2656ea5681736b671f0d71195f0
Andrey Popov <dil2232@gmail.com>
2025-11-05 12:10:06 +0600

    fix: fixed first game start and styles

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -2,7 +2,6 @@ import { Game } from '../components/Game/Game';
 
 function GamePage() {
   return <Game />;
-  // return <Board rows={6} cols={7} />;
 }
 
 export default GamePage;
--- src/pages/menu/MenuPage.tsx
+++ src/pages/menu/MenuPage.tsx
@@ -1,12 +1,18 @@
 import { useNavigate } from 'react-router';
+import rootStore from '../../stores/root.store';
+import { COLS, ROWS } from '../../types/gameTypes';
 import styles from './Menu.module.css';
 
 function MenuPage() {
   const navigate = useNavigate();
+  const startGame = () => {
+    rootStore.gameStore.startNewGame(ROWS, COLS);
+    navigate('/game');
+  };
   return (
     <div>
       <h1 className={styles.title}>Connect Four!</h1>
-      <button className={styles.start} onClick={() => navigate('/game')}>
+      <button className={styles.start} onClick={() => startGame()}>
         Начать игру
       </button>
     </div>
